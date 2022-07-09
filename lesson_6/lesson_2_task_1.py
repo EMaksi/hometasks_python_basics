54f2380624831504ea514511ef520a54170d8313
EMaksi <83609326+EMaksi@users.noreply.github.com>
2022-07-09 11:33:11 +0200

    Delete lesson_6 directory

--- lesson_6/lesson_2_task_1.py
+++
@@ -1,9 +0,0 @@
-my_list = [1, 2, -2.1, "Knopka", True, None]
-for el in my_list:
-    print(type(el))
-
-#for el in my_list:
-  #  even_el =
-
-
-
--- lesson_6/lesson_2_task_2.py
+++
@@ -1,6 +0,0 @@
-my_list_int_only = [1, 9, 54 , 0.4, -1, 4, -2]
-
-for i in range(1, len(my_list_int_only), 2):
-    my_list_int_only[i - 1],  my_list_int_only[i] = my_list_int_only[i], my_list_int_only[i - 1]
-
-print(my_list_int_only)
