163c07d87db4a157d983c9303811d40df1abe04e
EMaksi <83609326+EMaksi@users.noreply.github.com>
2022-07-09 11:24:47 +0200

    Delete lesson_1_task_3.py

--- lesson_1_task_3.py
+++
@@ -1,6 +0,0 @@
-n = (input("Enter an integer "))
-m = int(n) + int(n + n) + int (n +n +n) # my solution
-print(m)
-# решение преподавателя с форматированием : print(f"[n] + [n + n] + [n + n + n] = [int(n) + int(n + n) + int(n + n + n)]")
-
-
